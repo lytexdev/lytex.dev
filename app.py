c3f015e35f2be1fe940f828390840a6aea6bc74b
Immanuel Mruk <immanuelmruk@gmail.com>
2024-11-05 20:52:41 +0100

    Add next & previous control

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, send_from_directory, abort
+from flask import Flask, jsonify, render_template, send_from_directory, abort, url_for
 import os
 import json
 from markdown2 import markdown
@@ -20,11 +20,12 @@ def index():
 @app.route('/<path:subpath>')
 def show_article(subpath):
     metadata = load_metadata()
-    article = next((item for item in metadata if item["filepath"] == subpath), None)
+    article_index = next((index for index, item in enumerate(metadata) if item["filepath"] == subpath), None)
     
-    if not article:
+    if article_index is None:
         return abort(404)
-    
+
+    article = metadata[article_index]
     md_path = os.path.join(ARTICLES_PATH, f"{subpath}.md")
     if not os.path.exists(md_path):
         return abort(404)
@@ -32,9 +33,18 @@ def show_article(subpath):
     with open(md_path, 'r') as f:
         content = f.read()
         html_content = markdown(content, extras=['fenced-code-blocks'])
-    
-    return render_template('article.html', title=article['title'], content=html_content)
 
+    prev_article = metadata[article_index - 1] if article_index > 0 else None
+    next_article = metadata[article_index + 1] if article_index < len(metadata) - 1 else None
+    
+    return render_template(
+        'article.html',
+        title=article['title'],
+        content=html_content,
+        created_at=article['created_at'],
+        prev_article=prev_article,
+        next_article=next_article
+    )
 
 if __name__ == "__main__":
     try:
